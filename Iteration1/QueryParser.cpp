12c68425e312498835b17c548db048f571b82e03
nuruddinmazlan <nuruddinmazlan@gmail.com>
2015-09-02 15:25:53 +0800

    Update QueryParser.cpp with main function

--- Iteration1/QueryParser.cpp
+++ Iteration1/QueryParser.cpp
@@ -12,6 +12,56 @@ class QueryParser{
 	//method to add to the querytree
 	//method to add to the symbol table
 
+void Match (char *c)
+{
+	printf( " %s\n", c );
+}
+void GetNextToken()
+{
+
+}
+
+int main()
+{
+	//cout << "Hello World!";
+	//char str[80] = "Select s from something";
+	const char* s = " ";
+	char* token;
+
+	string i ;
+	getline (cin, i);
+	char *a=new char[i.size()+1];
+	a[i.size()]=0;
+	memcpy(a,i.c_str(),i.size());
+	char car; 
+
+	token = strtok(a,s);
+	bool foundID = false;
+	bool foundSelect = false;
+
+	/* get the first token */
+   //token = strtok(str, s);
+   
+   /* walk through other tokens */
+   while( token != NULL ) 
+   {
+      printf( " %s\n", token );
+	  char c = *token;
+	  if (isdigit(c))
+	  {
+		  foundID = true;
+		  printf( " %s ii\n", token );
+	  }
+	  Match(token);
+      token = strtok(NULL, s);
+	  
+   }
+   int tt;
+   cin >>  tt;
+
+	
+	return 0;
+}
 
 }
 
