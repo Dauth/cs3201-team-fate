c2485cc67b820d4f8e0f788eef7e410d7d9c1f5b
gary <garywjd91@gmail.com>
2015-09-04 22:08:03 +0800

    expressiontree.h

---
+++ Iteration1/ExpressionTree.h
@@ -0,0 +1,26 @@
+#ifndef Express
+#define PROCEDURETABLE_H
+
+#include "stdafx.h"
+#include "Node.h"
+
+
+
+class ExpressionTree{
+
+
+
+
+
+public:
+	std::vector <char> ExpressionTree::expressionConverter(string inflixString);
+	Node* ExpressionTree::exptreeSetup(vector<string> postflixExp, int lineNo);
+
+
+
+
+
+
+};
+
+#endif
