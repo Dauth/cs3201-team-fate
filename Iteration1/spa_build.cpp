d103946add7cfba2d867a8758f956bee1e0c4ec1
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2015-09-05 22:04:28 +0800

    fixed modifies,uses for container statements
    nested containers untested

--- Iteration1/spa_build.cpp
+++ Iteration1/spa_build.cpp
@@ -16,7 +16,7 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	proc->setLeftChild(testStmtLst1);
 
 	// 1. y = 3
-	Node* s1 = pkb.createNode(assignment, 1);
+	Node* s1 = pkb.createNode(assignment, 1, "", nullptr, nullptr, nullptr, proc);
 	Node* s1a = pkb.createNode(variable, 1, "y", nullptr, s1, nullptr, proc);
 	Node* s1c3 = pkb.createNode(constant, 1, "3");
 	s1->setLeftChild(s1a);
@@ -24,7 +24,7 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	testStmtLst1->addStmt(s1);
 
 	// 2. x = 4
-	Node* s2 = pkb.createNode(assignment, 2);
+	Node* s2 = pkb.createNode(assignment, 2, "", nullptr, nullptr, nullptr, proc);
 	Node* s2a = pkb.createNode(variable, 2, "x", nullptr, s2, nullptr, proc);
 	Node* s2c4 = pkb.createNode(constant, 2, "4");
 	s2->setLeftChild(s2a);
@@ -32,7 +32,7 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	testStmtLst1->addStmt(s2);
 
 	// 3. z = x + y
-	Node* s3 = pkb.createNode(assignment, 3);
+	Node* s3 = pkb.createNode(assignment, 3, "", nullptr, nullptr, nullptr, proc);
 	Node* s3a = pkb.createNode(variable, 3, "z", nullptr, s3, nullptr, proc);
 	Node* s3v_x = pkb.createNode(variable, 3, "x", s3, nullptr, nullptr, proc);
 	Node* s3e_plus = pkb.createNode(expression, 3, "+", s3, nullptr, nullptr, proc);
@@ -44,7 +44,7 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	testStmtLst1->addStmt(s3);
 
 	// 4. While x {
-	Node* s4 = pkb.createNode(whileLoop, 4);
+	Node* s4 = pkb.createNode(whileLoop, 4, "", nullptr, nullptr, nullptr, proc);
 	Node* s4v_x = pkb.createNode(variable, 4, "x", s4, nullptr, nullptr, proc);
 	Node* whileStmtLst = pkb.createNode(statementList, 4);
 	s4->setLeftChild(s4v_x);
@@ -53,12 +53,12 @@ int _tmain(int argc, _TCHAR* argv[]) {
 
 	// 5. a = z + y + x
 	Node* s5 = pkb.createNode(assignment, 5, "", nullptr, nullptr, s4, proc);
-	Node* s5a = pkb.createNode(variable, 5, "a", nullptr, s5, nullptr, proc);
-	Node* s5v_z = pkb.createNode(variable, 5, "z", s5, nullptr, nullptr, proc);
+	Node* s5a = pkb.createNode(variable, 5, "a", nullptr, s5, s4, proc);
+	Node* s5v_z = pkb.createNode(variable, 5, "z", s5, nullptr, s4, proc);
 	Node* s5e_plus1 = pkb.createNode(expression, 5, "+", s5, nullptr, nullptr, proc);
-	Node* s5v_y = pkb.createNode(variable, 5, "y", s5, nullptr, nullptr, proc);
+	Node* s5v_y = pkb.createNode(variable, 5, "y", s5, nullptr, s4, proc);
 	Node* s5e_plus2 = pkb.createNode(expression, 5, "+", s5, nullptr, nullptr, proc);
-	Node* s5v_x = pkb.createNode(variable, 5, "x", s5, nullptr, nullptr, proc);
+	Node* s5v_x = pkb.createNode(variable, 5, "x", s5, nullptr, s4, proc);
 	s5->setLeftChild(s5a);
 	s5->setRightChild(s5e_plus1);
 	s5e_plus1->setLeftChild(s5e_plus2);
@@ -69,10 +69,10 @@ int _tmain(int argc, _TCHAR* argv[]) {
 
 	// 6. b = a + z
 	Node* s6 = pkb.createNode(assignment, 6, "", nullptr, nullptr, s4, proc);
-	Node* s6a = pkb.createNode(variable, 6, "b", nullptr, s6, nullptr, proc);
-	Node* s6v_a = pkb.createNode(variable, 6, "a", s6, nullptr, nullptr, proc);
+	Node* s6a = pkb.createNode(variable, 6, "b", nullptr, s6, s4, proc);
+	Node* s6v_a = pkb.createNode(variable, 6, "a", s6, nullptr, s4, proc);
 	Node* s6e_plus = pkb.createNode(expression, 6, "+", s6, nullptr, nullptr, proc);
-	Node* s6v_z = pkb.createNode(variable, 6, "z", s6, nullptr, nullptr, proc);
+	Node* s6v_z = pkb.createNode(variable, 6, "z", s6, nullptr, s4, proc);
 	s6->setLeftChild(s6a);
 	s6->setRightChild(s6e_plus);
 	s6e_plus->setLeftChild(s6v_a);
@@ -81,10 +81,10 @@ int _tmain(int argc, _TCHAR* argv[]) {
 
 	// 7. c = z + x }
 	Node* s7 = pkb.createNode(assignment, 7, "", nullptr, nullptr, s4, proc);
-	Node* s7a = pkb.createNode(variable, 7, "c", nullptr, s7, nullptr, proc);
-	Node* s7v_z = pkb.createNode(variable, 7, "z", s7, nullptr, nullptr, proc);
+	Node* s7a = pkb.createNode(variable, 7, "c", nullptr, s7, s4, proc);
+	Node* s7v_z = pkb.createNode(variable, 7, "z", s7, nullptr, s4, proc);
 	Node* s7e_plus = pkb.createNode(expression, 7, "+", s7, nullptr, nullptr, proc);
-	Node* s7v_x = pkb.createNode(variable, 7, "x", s7, nullptr, nullptr, proc);
+	Node* s7v_x = pkb.createNode(variable, 7, "x", s7, nullptr, s4, proc);
 	s7->setLeftChild(s7a);
 	s7->setRightChild(s7e_plus);
 	s7e_plus->setLeftChild(s7v_z);
@@ -92,7 +92,7 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	whileStmtLst->addStmt(s7);
 
 	// 8. x = x + z
-	Node* s8 = pkb.createNode(assignment, 8);
+	Node* s8 = pkb.createNode(assignment, 8, "", nullptr, nullptr, nullptr, proc);
 	Node* s8a = pkb.createNode(variable, 8, "x", nullptr, s8, nullptr, proc);
 	Node* s8v_x = pkb.createNode(variable, 8, "x", s8, nullptr, nullptr, proc);
 	Node* s8e_plus = pkb.createNode(expression, 8, "+", s8, nullptr, nullptr, proc);
@@ -104,7 +104,7 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	testStmtLst1->addStmt(s8);
 
 	// 9. a = 3 + y
-	Node* s9 = pkb.createNode(assignment, 9);
+	Node* s9 = pkb.createNode(assignment, 9, "", nullptr, nullptr, nullptr, proc);
 	Node* s9a = pkb.createNode(variable, 9, "a", nullptr, s9, nullptr, proc);
 	Node* s9c_3 = pkb.createNode(constant, 9, "3");
 	Node* s9e_plus = pkb.createNode(expression, 9, "+", s9, nullptr, nullptr, proc);
@@ -139,14 +139,6 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	std::cout << "Obtained " << result.size() << " results\n"; // 8
 	for(int i=0; i<result.size(); i++) {
 		std::cout << "( " << result[i]->getLine() << ", " << result[i]->getVariable()->getName() << " ) ";
-	} 
-
-	// Modifies (s/p, "y")
-	std::cout << "\n\n\Query is Modifies(s/p, \"y\") \n"; 
-	result = pkb.getModifiedBy("y");
-	std::cout << "Obtained " << result.size() << " results\n"; // 9
-	for(int i=0; i<result.size(); i++) {
-		std::cout << "( " << result[i]->getLine() << ", \"y\" ) ";
 	} 
 
 	// Modifies (s/p, "y")
