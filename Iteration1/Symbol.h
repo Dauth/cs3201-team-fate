7e9656a6e24a6b8c5a4f7dbf1b1726e22b349269
Nuruddin <nuruddinmazlan@gmail.com>
2015-09-06 02:02:30 +0800

    Include exists function to check if variable exists in the symboltable

--- Iteration1/Symbol.h
+++ Iteration1/Symbol.h
@@ -9,13 +9,15 @@ private:
 	std::vector<Data*> symbolTable;
 
 public:
+	Symbol();
 	Symbol(std::string var, synt_type varType);
 	//attributes
 	void setVar(std::string var, synt_type varType);
 	void setResult(int index);
-	std::vector<Data*>& getQuery();
+	std::vector<Data*> getQuery();
 	int getSize();
 	int getIndex(std::string var);
+	bool exists(std::string var);
 };
 
 #endif
