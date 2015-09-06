69b9f9dabff9df070e4dfea493157dc99162fefb
Nuruddin <nuruddinmazlan@gmail.com>
2015-09-06 22:39:36 +0800

    Add new method for QueryParser

--- Iteration1/Symbol.h
+++ Iteration1/Symbol.h
@@ -14,10 +14,11 @@ public:
 	//attributes
 	void setVar(std::string var, synt_type varType);
 	void setResult(int index);
-	std::vector<Data*>& getQuery();
+	std::vector<Data*> getQuery();
 	int getSize();
 	int getIndex(std::string var);
 	bool exists(std::string var);
+	synt_type getSyntType(std::string var);
 };
 
 #endif
